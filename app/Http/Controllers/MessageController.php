c227ee9eaf8c3d5210b6a1f722f6d9a39ce96915
osvaldom <mekymaria@gmail.com>
2021-03-16 16:18:57 +0200

    Revert "Add Test for invalid email"
    This reverts commit 61fbf94a511f5662b0e5fc892bd94546c2f1d607.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attachment;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
-class AttachmentController extends Controller
+class MessageController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,17 +14,7 @@ class AttachmentController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json(Message::all());
     }
 
     /**
@@ -41,21 +31,10 @@ class AttachmentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Attachment  $attachment
+     * @param  \App\Models\Message  $message
      * @return \Illuminate\Http\Response
      */
-    public function show(Attachment $attachment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Attachment  $attachment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Attachment $attachment)
+    public function show(Message $message)
     {
         //
     }
@@ -64,10 +43,10 @@ class AttachmentController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Attachment  $attachment
+     * @param  \App\Models\Message  $message
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Attachment $attachment)
+    public function update(Request $request, Message $message)
     {
         //
     }
@@ -75,10 +54,10 @@ class AttachmentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Attachment  $attachment
+     * @param  \App\Models\Message  $message
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Attachment $attachment)
+    public function destroy(Message $message)
     {
         //
     }
--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attachment;
+use App\Models\Status;
 use Illuminate\Http\Request;
 
-class AttachmentController extends Controller
+class StatusController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,16 +13,6 @@ class AttachmentController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
         //
     }
@@ -41,21 +31,10 @@ class AttachmentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Attachment  $attachment
+     * @param  \App\Models\Status  $status
      * @return \Illuminate\Http\Response
      */
-    public function show(Attachment $attachment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Attachment  $attachment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Attachment $attachment)
+    public function show(Status $status)
     {
         //
     }
@@ -64,10 +43,10 @@ class AttachmentController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Attachment  $attachment
+     * @param  \App\Models\Status  $status
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Attachment $attachment)
+    public function update(Request $request, Status $status)
     {
         //
     }
@@ -75,10 +54,10 @@ class AttachmentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Attachment  $attachment
+     * @param  \App\Models\Status  $status
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Attachment $attachment)
+    public function destroy(Status $status)
     {
         //
     }
